7aaf4344ffae6f8ae4856c9aea59eaea925d4eb9
Kat Wilson <katjanewilson@gmail.com>
2021-02-09 15:33:19 +0000

    edits

--- r_tutorial_mle42stan.R
+++ r_tutorial_mle42stan.R
@@ -1,12 +1,18 @@
 ##install packages
 
 install.packages('devtools')
-install_github('rmcelreath/glmer2stan')
+install.packages("remotes")
+remotes::install_github("rmcelreath/glmer2stan")
+devtools::install_github('rmcelreath/glmer2stan')
+usethis::browse_github_pat()
+usethis::edit_r_environ()
+usethis::edit_r_environ(token = 59336fb94563b0b475e5bb26b57c6641632a4178)
 source('http://mc-stan.org/rstan/install.R', echo = TRUE, max.deparse.length = 2000)
 install.packages("rstan")
 source_url("https://github.com/stan-dev/shinystan/raw/develop/install_shinystan.R")
 install_shinystan()
 install.packages('lmerTest')
+install.packages("glmer2stan")
 
 
 library(devtools)
@@ -73,8 +79,59 @@ print(fit1)
 
 
 
-################ lme4_to_stan
+################ glmer2stan
 ##step 4
 ################
+
+
+data("sleepstudy")
+#days nested within subjects
+#so you can look at both random slopes and intercepts
+data(sleepstudy)
+
+
+# plot effects first!
+
+# no random effects model
+m1_lm <- lm(Reaction~Days,data=sleepstudy)
+confint(m1_lm)
+summary(m1_lm)
+
+ggplot(sleepstudy,aes(x=Days,y=Reaction))+
+  geom_point()+
+  guides(color=F)+
+  geom_smooth(method=lm,se = F)
+
+
+# with nesting of intercepts and slopes within subjs
+ggplot(sleepstudy,aes(x=Days,y=Reaction,color=Subject,group=Subject))+
+  geom_point()+
+  guides(color=F)+
+  geom_smooth(method=lm,se = F)
+
+# model in regular ol' lme4, random slopes and intercepts set
+m1_lme4 <- lmer( Reaction ~ Days + (Days | Subject), sleepstudy, REML=FALSE )
+summary(m1_lme4)
+confint(m1_lme4)
+
+AIC(m1_lm,m1_lme4) # fixed effects the same, model is more parsimonious i.e. random effects = good
+
+
+
+# convert factor of subject ids to sequential integers
+sleepstudy$subject_index <- as.integer(as.factor(sleepstudy$Subject)) 
+
+# basic MCMC parameter, should probably be a bit higher but we dont have all day!
+nwarm = 100 # burn-in period, these samples are not included in estimation
+niter = 500 # number of steps per chain, more is better (but takes longer)
+chains = 4 # number of chains, usually at least 2
+
+m1_g2s <- lmer2stan( Reaction ~ Days + (Days | subject_index), data=sleepstudy,
+                     calcWAIC=T,
+                     warmup=nwarm, 
+                     iter = niter, 
+                     chains=chains) 
+
+
 
 
